7700488a05566d3bcc5a31bd908d8e7880b32d89
Scott Sinclair <scott.sinclair.za@gmail.com>
2017-07-08 11:42:08 +0200

    Rename package

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-import pymsg
+import lsasaf
 
 coff = 1856
 loff = 1856
@@ -6,10 +6,10 @@ loff = 1856
 print('\n\n### Geo location supplied\n\n')
 
 lat, lon = (-22.0, 14.97)
-row, col = pymsg.geoloc_to_pixelloc(lat, lon, loff, coff)
+row, col = lsasaf.geoloc_to_pixelloc(lat, lon, loff, coff)
 
 # Reverse the transform
-lat1, lon1 = pymsg.pixelloc_to_geoloc(row, col, loff, coff)
+lat1, lon1 = lsasaf.pixelloc_to_geoloc(row, col, loff, coff)
 
 print('Requested location (lat, lon) = ({:6f}, {:6f})...'.format(lat, lon))
 
@@ -19,10 +19,10 @@ print('Calculated location (lat, lon) = ({:6f}, {:6f})...'.format(lat1, lon1))
 print('\n\n### Pixel location supplied\n\n')
 
 row, col = (2631, 2356)
-lat1, lon1 = pymsg.pixelloc_to_geoloc(row, col, loff, coff)
+lat1, lon1 = lsasaf.pixelloc_to_geoloc(row, col, loff, coff)
 
 # Reverse the transform
-row1, col1 = pymsg.geoloc_to_pixelloc(lat1, lon1, loff, coff)
+row1, col1 = lsasaf.geoloc_to_pixelloc(lat1, lon1, loff, coff)
 
 print('Requested pixel is at (row, col) = ({:d}, {:d})...'.format(row, col))
 
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, Extension
 from Cython.Distutils import build_ext
 
 setup(
-    name='pymsg',
+    name='lsasaf',
     version='0.1',
     cmdclass={'build_ext': build_ext},
-    ext_modules=[Extension('pymsg._nav',
-                           ['pymsg/_nav.pyx', 'pymsg/MSG_navigation.c'],
+    ext_modules=[Extension('lsasaf._nav',
+                           ['lsasaf/_nav.pyx', 'lsasaf/MSG_navigation.c'],
                            libraries=['m'],
                            extra_compile_args=['-O3'])
                  ],
-    packages=['pymsg']
+    packages=['lsasaf']
 )
